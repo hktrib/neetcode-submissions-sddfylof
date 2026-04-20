472ba045b4467fa76f49aca72057a46beeded832
Apramay Singh <116051160+hktrib@users.noreply.github.com>
2026-04-20 13:40:53 -0700

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.py
@@ -0,0 +1,57 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        # A: we are looking to see if the graph is connected
+        #       does a graph traversal hit n unique nodes 
+        #           len(visited) == n then OK
+
+        # B: We are looking to see if the graph is a valid tree
+        #       No Cycles or Loops
+        #           get around undirected graph edge case with prev node
+        #
+
+        # Let's do a DFS as well to test
+
+        if not n:
+            return True
+
+        adj = { i:[] for i in range(n)}
+
+        for vertex, connectedVertex in edges:
+            adj[vertex].append(connectedVertex)
+            adj[connectedVertex].append(vertex)
+
+        visited = set()
+
+        def dfs(curr : int, prev: int):
+            if curr in visited:
+                return False
+
+            visited.add(curr)
+            for connectedNode in adj[curr]:
+                if connectedNode != prev:
+                    if not dfs(connectedNode, curr):
+                        return False
+            return True
+
+        
+
+
+        # Problem here is that:
+        '''
+            If the graph is a tree, the first call will visit every node
+            If there's left over nodes its not a tree in the first place
+
+        '''
+    
+        if dfs(0, -1) and len(visited) == n:
+            return True
+        return False
+
+        
+
+
+
+        
+
+
+
