5e7de8182a61a82c9c546a668f82cf41cf82bbc3
Apramay Singh <116051160+hktrib@users.noreply.github.com>
2026-05-10 13:13:57 -0700

    Add: longest-palindromic-substring - submission-6

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-6.py
@@ -0,0 +1,37 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        # Memoization solution!!!!!
+
+        n = len(s)
+        if n < 2:
+            return s
+            
+        res = s[0]
+        resLen = 1
+        memo = [[None for _ in range(n)] for _ in range(n)]
+        
+
+        def checkPalindrome(i, j):
+            if i == j:
+                return True
+            elif memo[i][j] is not None:
+                return memo[i][j]
+            elif i + 1 == j:
+                return s[i] == s[j]
+            else:
+                # Recursive Step: 
+                # S[i..j] is a palindrome 
+                #   ONLY IF outer chars match 
+                #   AND the inner substring S[i+1..j-1] is a palindrome.
+                memo[i][j] = s[i] == s[j] and checkPalindrome(i + 1, j - 1)
+                return memo[i][j]
+        
+
+        for i in range(len(s)):
+            for j in range(i + 1, len(s)):
+                if checkPalindrome(i, j):
+                    if resLen < (j - i + 1):
+                        resLen = j - i + 1
+                        res = s[i: j + 1]
+
+        return res
