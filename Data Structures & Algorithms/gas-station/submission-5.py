b6a8e218ad05932682d9fefd04b750883d53005e
Apramay Singh <116051160+hktrib@users.noreply.github.com>
2026-05-18 19:10:38 -0700

    Add: gas-station - submission-5

---
+++ Data Structures & Algorithms/gas-station/submission-5.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        
+        if sum(gas) < sum(cost):
+            # Not Possible to complete circuit
+            return -1
+        else:
+            # Possible to complete circuit
+            index = 0
+            profit = 0
+            for i in range(len(gas)):
+                profit += (gas[i] - cost[i])
+                if profit < 0:
+                    index = i + 1
+                    profit = 0
+            return index
